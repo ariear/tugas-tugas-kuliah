e081009a33258f813d8a083453dc342b3e9ec037
ariear <arieinjector@gmail.com>
2025-10-01 08:40:59 +0700

    feat(add): some changes in landing page project

--- pweb/landing-page/script.js
+++ pweb/landing-page/script.js
@@ -1,3 +1,15 @@
+// navbar
+const navbar = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    navbar.classList.add("border-b", "border-[#F7C35F]");
+  } else {
+    navbar.classList.remove("border-b", "border-[#F7C35F]");
+  }
+});
+
+// projects slide
 const slider = document.getElementById("slider");
 const next = document.getElementById("next");
 const prev = document.getElementById("prev");
@@ -19,7 +31,28 @@ prev.addEventListener("click", () => {
   updateSlider();
 });
 
+// testimonials slide
+const sliderTesti = document.getElementById("slider-testi");
+const indicators = document.querySelectorAll("#indicators span");
+let indexTesti = 0;
+const totalSlidesTesti = sliderTesti.children.length;
+
+function showSlide(i) {
+  sliderTesti.style.transform = `translateX(-${i * 100}%)`;
+  indicators.forEach((dot, idx) => {
+    dot.classList.toggle("opacity-100", idx === i);
+    dot.classList.toggle("opacity-40", idx !== i);
+  });
+}
+
+function nextSlide() {
+  indexTesti = (indexTesti + 1) % totalSlidesTesti;
+  showSlide(indexTesti);
+}
+
 setInterval(() => {
   index = (index + 1) % totalSlides;
   updateSlider();
+
+  nextSlide();
 }, 3000);
